d568315f129f1b1dc7d05ca57b220a8dd780929d
papecheikh123 <papcheikh09@gmail.com>
2020-03-16 21:48:28 +0000

    injection jwtInterceptor

--- src/app/authentification.service.ts
+++ src/app/authentification.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from './../environments/environment';
 import { User } from './models/user';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,11 @@ export class AuthentificationService {
   constructor(private httpclient:HttpClient){}
   getConnexion(user:User){
     return this.httpclient.post<User>(`${environment.apiUrl}/login_check`,user);
-  }
+    pipe(map(user => {
+      // store user details and jwt token in local storage to keep user logged in between page refreshes
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.currentUserSubject.next(user);
+      return user;
+  }));
 }
+  }
